5e459e08b976af3670705f7126456847338e24cb
Reqima-dev <adloukondo@gmail.com>
2023-06-20 12:38:04 +0100

    update

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,9 +3,8 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  const res = await fetch(`https://bytebuzz.vercel.app/api/posts/${id}`, {
     cache: "no-store",
   });
 
@@ -21,7 +20,7 @@ export async function generateMetadata({ params }) {
   return {
     title: post.title,
     description: post.desc,
-  }
+  };
 }
 const BlogPost = async ({ params }) => {
   const data = await getData(params.id);
