7a2a3e1f305395359dd76256148620921bdd0ba2
Reqima-dev <adloukondo@gmail.com>
2023-06-20 13:10:07 +0100

    upadate one

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-    cache: "no-store",
-  });
+  // const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  //   cache: "no-store",
+  // });
 
   if (!res.ok) {
     return notFound();
